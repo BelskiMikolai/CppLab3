f694f07e2e5304b93628c3eac6880db4e9a19df2
BelskiMikolai <146476748+BelskiMikolai@users.noreply.github.com>
2023-10-20 10:48:13 +0300

    Create

---
+++ Lab3/Source.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+const double PI = 3.141592653589793;
+
+int main()
+{
+    //Создаём переменные 
+    double a, b, h;
+    int i = 1;
+    //Запрашиваем их
+    cout << "Enter a, b, h\n"; 
+    cin >> a >> b >> h; //0.7 1.8 0.1
+    cout <<"a: "<< a<<" b: " << b<<" h: " << h << "\n";
+    //Создаём переменные y и y_max, y_min для нахождения минимального/максимального
+    double y = -log(fabs(2 * sin(a / 2)));
+    double y_max = y;
+    double y_min = y;
+    //Цикл фор для нахождения значений у при изменении х от a до b.
+    for (double x = a; x < b; x+=h)
+    {
+        y = -log(fabs(2 * sin(x / 2)));
+
+        cout << i << ". " << "y= " << y << " " << "x= " << x << endl;
+        i++;
+        //Проверка на y_max, y_min
+        if (y > y_max)
+        {
+            y_max = y;
+        }
+        if (y < y_min)
+        {
+            y_min = y;
+        }
+    }
+    //Вывод
+    cout << "y_max: " << y_max << "\ny_min: " << y_min<<endl;
+	return 0;
+}
---
+++ Lab3_2/Source.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+const double PI = 3.141592653589793;
+
+int main()
+{
+	//Инициируем переменные 
+	double a, b, h, y, n;
+	double s = 0;
+	double r = -1;
+	int i = 1;
+	cout << "Enter a, b, h, n\n";
+	/*a = 0.1;aa
+	b = 1;
+	h = 0.1;
+	n = 5;*/
+	cin >> a >> b >> h >> n;
+	cout << "a: " << a << " b: " << b << " h: " << h << " n: " << n << "\n";
+	//Цикл фор для нахождения у и s с х, изменяющегося от a до b с шагом h
+	for (double x = a; x < b; x += h)
+	{
+		y = x * atan(x) - log(sqrt(1 + pow(x, 2)));
+		//Цикл фор для расчёта суммы s
+		for (double k = 1; k < n; k++)
+		{
+			r = -r * pow(x, 2);
+			s += r / (2 * k * (2 * k - 1));
+		}
+		//Выыод Y, S и |Y-S|
+		cout << left << setw(6) << i;
+		cout << left << setw(7) << "Y=";
+		cout << left << setw(10) << y;
+		cout << left << setw(7) << "S=";
+		cout << left << setw(10) << s;
+		cout << left << setw(10) << "|Y-S|=";
+		cout << left << setw(10) << fabs(y - s) << endl;
+
+		s = 0;
+		r = -1;
+		i++;
+	}
+
+	return 0;
+}
